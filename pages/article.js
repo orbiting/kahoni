d5e9c439a3636c63e17d6867fed27c00819e86ba
Daniel Pfänder <daniel.pfaender@project-r.construction>
2017-07-17 18:07:06 +0200

    Add article fields and basic typography

--- pages/article.js
+++ pages/article.js
@@ -5,10 +5,18 @@ import Loader from '../components/Loader'
 import { gql, graphql } from 'react-apollo'
 import withData from '../lib/withData'
 
+import { H1, H2, Interaction, Lead, P } from '@project-r/styleguide'
+
 const article = gql`
   query article($slug: String!) {
     Article(slug: $slug) {
+      author
+      body
+      comments {
+        body
+      }
       id
+      lead
       title
       slug
       dossiers {
@@ -28,19 +36,34 @@ const Article = graphql(article)(({ data: { loading, error, Article } }) => {
       render={() => {
         if (Article) {
           return (
-            <div>
-              <h1>
+            <article>
+              <H1>
                 {Article.title}
-              </h1>
-              <div>
+              </H1>
+              <P>
+                von {Article.author}
+              </P>
+              <Lead>
+                {Article.lead}
+              </Lead>
+              <P>
+                {Article.body}
+              </P>
+              <P>
                 Dossier:&nbsp;
                 {Article.dossiers.map(dossier =>
                   <Link route="dossier" params={{ slug: dossier.slug }}>
                     {dossier.title}
                   </Link>
                 )}
-              </div>
-            </div>
+              </P>
+              <Interaction.H2>Kommentare</Interaction.H2>
+              {Article.comments.map(comment =>
+                <Interaction.P>
+                  {comment.body}
+                </Interaction.P>
+              )}
+            </article>
           )
         }
       }}
