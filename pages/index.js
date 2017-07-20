1a3fc31001c848c60328d0c6ab89102127a89871
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-07-20 14:59:33 +0200

    add auth components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import withData from '../lib/withData'
 import Frame from '../components/Frame'
+import Me from '../components/Auth/Me'
 
 import { H1 } from '@project-r/styleguide'
 
 export default withData(({ url }) =>
   <Frame url={url}>
     <H1>Es ist Zeit.</H1>
+    <Me />
   </Frame>
 )
