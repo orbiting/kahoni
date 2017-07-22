ff3b89d87d5d36e54be65885104877f9d58d4f0f
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-07-22 18:38:23 +0200

    add missing with data

--- pages/join.js
+++ pages/join.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import App from '../components/App'
 import Frame from '../components/Frame'
+import withData from '../lib/withData'
 
-export default ({ url }) =>
-	<Frame url={url}>
-		<article>
-			<h1>Mitmachen</h1>
-		</article>
-	</Frame>
+export default withData(({ url }) =>
+  <Frame url={url}>
+    <article>
+      <h1>Mitmachen</h1>
+    </article>
+  </Frame>
+)
