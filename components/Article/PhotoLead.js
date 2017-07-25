f296795ac7f3a1e3f0defefaba2c6a8c4a8ace81
Daniel Pfänder <daniel.pfaender@project-r.construction>
2017-07-25 11:11:20 +0200

    Move photo lead to separate component so we can use it on the feed page

---
+++ components/Article/PhotoLead.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+import ActionBar from '../ActionBar/'
+import Author from './Author'
+import Time from './Time'
+import Body from './Body'
+
+import { css } from 'glamor'
+
+import { Link } from '../../routes'
+
+import { H1, mediaQueries } from '@project-r/styleguide'
+
+const styles = {
+  cover: css({
+    backgroundColor: '#ddd',
+    backgroundSize: 'cover',
+    minHeight: '500px',
+    padding: '50px 20px 180px 20px',
+    position: 'relative',
+    textAlign: 'center',
+    [mediaQueries.onlyS]: {
+      margin: '0 -20px'
+    }
+  }),
+  opaqueHelper: css({
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    zIndex: 0
+  }),
+  meta: css({
+    bottom: '20px',
+    left: 0,
+    position: 'absolute',
+    right: 0
+  })
+}
+
+const PhotoLead = ({ article }) => {
+  const coverPhoto = article.photos[0]
+  const coverPhotoStyle = coverPhoto
+    ? { backgroundImage: 'url(' + coverPhoto.file.url + ')' }
+    : ''
+  let opaqueColor = ''
+  let textColor = '#000'
+  if (article.theme == 'DARK') {
+    opaqueColor = 'rgba(0,0,0,.25)'
+    textColor = '#fff'
+  } else if (article.theme == 'LIGHT') {
+    opaqueColor = 'rgba(255,255,255,.75)'
+  }
+  let photos = article.photos.length ? [...article.photos.slice(1)] : []
+
+  return (
+    <div {...styles.cover} style={coverPhotoStyle}>
+      {opaqueColor &&
+        <div
+          {...styles.opaqueHelper}
+          style={{ backgroundColor: opaqueColor }}
+        />}
+      <H1
+        style={{
+          position: 'relative',
+          zIndex: 3,
+          color: textColor
+        }}
+      >
+        {article.title}
+      </H1>
+      <div {...styles.meta}>
+        <Author name={article.author} showBadge={true} color={textColor} />
+        <Time
+          date={article.updatedAt}
+          readingMinutes={article.readingMinutes}
+          color={textColor}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default PhotoLead
--- components/Article/ReportageFormat.js
+++ components/Article/ReportageFormat.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import ActionBar from '../ActionBar/'
-import Author from './Author'
-import Time from './Time'
+import PhotoLead from './PhotoLead'
 import SharedBy from './SharedBy'
 import Body from './Body'
 
@@ -23,31 +22,6 @@ import {
 } from '@project-r/styleguide'
 
 const styles = {
-  cover: css({
-    backgroundColor: '#ddd',
-    backgroundSize: 'cover',
-    minHeight: '500px',
-    padding: '50px 20px 180px 20px',
-    position: 'relative',
-    textAlign: 'center',
-    [mediaQueries.onlyS]: {
-      margin: '0 -20px'
-    }
-  }),
-  opaqueHelper: css({
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-    zIndex: 0
-  }),
-  meta: css({
-    bottom: '20px',
-    left: 0,
-    position: 'absolute',
-    right: 0
-  }),
   photo: css({
     backgroundSize: 'cover',
     height: '250px',
@@ -56,46 +30,10 @@ const styles = {
 }
 
 const ReportageFormat = ({ article, me, share }) => {
-  const coverPhoto = article.photos[0]
-  const coverPhotoStyle = coverPhoto
-    ? { backgroundImage: 'url(' + coverPhoto.file.url + ')' }
-    : ''
-  let opaqueColor = ''
-  let textColor = '#000'
-  if (article.theme == 'DARK') {
-    opaqueColor = 'rgba(0,0,0,.25)'
-    textColor = '#fff'
-  } else if (article.theme == 'LIGHT') {
-    opaqueColor = 'rgba(255,255,255,.75)'
-  }
   let photos = article.photos.length ? [...article.photos.slice(1)] : []
-
   return (
     <article>
-      <div {...styles.cover} style={coverPhotoStyle}>
-        {opaqueColor &&
-          <div
-            {...styles.opaqueHelper}
-            style={{ backgroundColor: opaqueColor }}
-          />}
-        <H1
-          style={{
-            position: 'relative',
-            zIndex: 3,
-            color: textColor
-          }}
-        >
-          {article.title}
-        </H1>
-        <div {...styles.meta}>
-          <Author name={article.author} showBadge={true} color={textColor} />
-          <Time
-            date={article.updatedAt}
-            readingMinutes={article.readingMinutes}
-            color={textColor}
-          />
-        </div>
-      </div>
+      <PhotoLead article={article} />
       {me &&
         <ActionBar
           url={
